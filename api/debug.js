6a41baf2e5d16d63479b871523e14d97e37dcc6a
zimanovich87-tech <zimanovich87@gmail.com>
2025-11-14 12:13:11 +0300

    Delete api/debug.js

--- api/debug.js
+++
@@ -1,8 +0,0 @@
-export default function handler(request, response) {
-  response.status(200).json({
-    message: "DEBUG WORKS!",
-    timestamp: new Date().toISOString(),
-    method: request.method,
-    url: request.url
-  });
-}
